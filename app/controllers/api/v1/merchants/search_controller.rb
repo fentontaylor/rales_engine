a9ce6fdb0267945e082e0fc88af7d2bb2355f5f6
Fenton Taylor <fentontaylor@gmail.com>
2019-10-04 07:43:23 -0600

    Refactor date searching

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -5,10 +5,13 @@ class Api::V1::Merchants::SearchController < ApplicationController
       Merchant.find_by( search_params )
     when 'name'
       Merchant.find_by_lower_name( search_params[:name], all: false )
-    when 'created_at'
-      Merchant.find_by_flex_date(created_at: search_params[:created_at])
-    when 'updated_at'
-      Merchant.find_by_flex_date(updated_at: search_params[:updated_at])
+    when 'created_at', 'updated_at'
+      # Merchant.find_by_flex_date(created_at: search_params[:created_at])
+      Merchant.where( search_params ).first
+    # when 'updated_at'
+    #   # Merchant.find_by_flex_date(updated_at: search_params[:updated_at])
+    #   Merchant.where( search_params ).first
+
     end
     render json: MerchantSerializer.new( merchant )
   end
@@ -16,10 +19,17 @@ class Api::V1::Merchants::SearchController < ApplicationController
   def index
     merchant = case search_params.keys.first
     when 'id'
-      Merchant.all_by_id( search_params[:id] )
+      Merchant.where( search_params )
     when 'name'
-    when 'created_at'
-    when 'updated_at'
+      Merchant.find_by_lower_name( search_params[:name] )
+    when 'created_at', 'updated_at'
+      # Merchant.find_by_flex_date(created_at: search_params[:created_at])
+      Merchant.where( search_params )
+
+    # when 'updated_at'
+    #   # Merchant.find_by_flex_date(updated_at: search_params[:updated_at])
+    #   Merchant.where( search_params )
+
     end
     render json: MerchantSerializer.new( merchant )
   end
--- spec/factories/merchant.rb
+++ spec/factories/merchant.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :merchant do
     sequence(:name) { |x| "Bob Ross Paints #{x}" }
+    created_at { Time.now }
+    updated_at { created_at }
   end
 end
