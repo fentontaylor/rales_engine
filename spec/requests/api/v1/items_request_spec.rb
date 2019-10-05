dbdb05bc3396a43592c7521a27027b01c4c76d2a
Fenton Taylor <fentontaylor@gmail.com>
2019-10-05 13:08:20 -0600

    Add endpoint /api/v1/items/most_revenue?quantity=x

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -36,6 +36,66 @@ describe 'Items API' do
   end
 
   it 'can return the top x items ranked by total revenue' do
-    
+    item_1 = create(:item, unit_price: 1000)
+    item_2 = create(:item, unit_price: 1000)
+    item_3 = create(:item, unit_price: 1000)
+    item_4 = create(:item, unit_price: 1000)
+
+    # item_1: total_revenue = 2000
+    invoice_1 = create(:invoice)
+    invoice_2 = create(:invoice)
+    create(:invoice_item, invoice: invoice_1, item: item_1, quantity: 1)
+    create(:invoice_item, invoice: invoice_2, item: item_1, quantity: 1)
+    create(:transaction, invoice: invoice_1)
+    create(:transaction, invoice: invoice_2)
+
+    # item_2: total_revenue = 3000
+    invoice_3 = create(:invoice)
+    invoice_4 = create(:invoice)
+    invoice_7 = create(:invoice)
+    create(:invoice_item, invoice: invoice_3, item: item_2, quantity: 1)
+    create(:invoice_item, invoice: invoice_4, item: item_2, quantity: 2)
+    create(:invoice_item, invoice: invoice_7, item: item_2, quantity: 10)
+    create(:transaction, invoice: invoice_3)
+    create(:transaction, invoice: invoice_4)
+    create(:transaction, invoice: invoice_7, result: 'failed')
+
+    # item_3: total_revenue = 5000
+    invoice_5 = create(:invoice)
+    invoice_6 = create(:invoice)
+    create(:invoice_item, invoice: invoice_5, item: item_3, quantity: 3)
+    create(:invoice_item, invoice: invoice_6, item: item_3, quantity: 2)
+    create(:transaction, invoice: invoice_5)
+    create(:transaction, invoice: invoice_6)
+
+    # item_4: total_revenue = 4000
+    invoice_8 = create(:invoice)
+    invoice_9 = create(:invoice)
+    invoice_10 = create(:invoice)
+    create(:invoice_item, invoice: invoice_8, item: item_4, quantity: 2)
+    create(:invoice_item, invoice: invoice_9, item: item_4, quantity: 2)
+    create(:invoice_item, invoice: invoice_10, item: item_4, quantity: 8)
+    create(:transaction, invoice: invoice_8)
+    create(:transaction, invoice: invoice_9)
+    create(:transaction, invoice: invoice_10, result: 'failed')
+
+    get "/api/v1/items/most_revenue?quantity=1"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json['data'].first['id']).to eq(item_3.id.to_s)
+
+
+    get "/api/v1/items/most_revenue?quantity=3"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json['data'][0]['id']).to eq(item_3.id.to_s)
+    expect(json['data'][1]['id']).to eq(item_4.id.to_s)
+    expect(json['data'][2]['id']).to eq(item_2.id.to_s)
   end
 end
