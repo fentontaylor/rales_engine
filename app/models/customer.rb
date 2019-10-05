35476fa0910479debd7aea679492f1457d7e23f4
Fenton Taylor <fentontaylor@gmail.com>
2019-10-05 16:49:35 -0600

    Add Customer#favorite_merchant to return the merchant for which a customer has most transactions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,18 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
+
+  def favorite_merchant
+    Merchant.find_by_sql(
+      "SELECT id, name, created_at, updated_at from " +
+        "(SELECT m.*, COUNT(*) as num_transactions FROM customers c " +
+        "INNER JOIN invoices i ON c.id = i.customer_id " +
+        "INNER JOIN transactions t ON t.invoice_id = i.id " +
+        "INNER JOIN merchants m ON i.merchant_id = m.id " +
+        "WHERE t.result = 'success' AND c.id = #{self.id} " +
+        "GROUP BY m.id " +
+        "ORDER BY num_transactions DESC " +
+        "LIMIT 1) temp"
+    ).first
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -12,4 +12,33 @@ describe Customer do
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe 'instance methods' do
+    it '#favorite merchant' do
+      bob = create(:customer, first_name: 'Bob')
+
+      # merchant_1: num_transactions = 1
+      merchant_1 = create(:merchant)
+      invoice_1 = create(:invoice, customer: bob, merchant: merchant_1)
+      create(:transaction, invoice: invoice_1)
+
+      # merchant_2: num_transactions = 2
+      merchant_2 = create(:merchant)
+      invoice_2 = create(:invoice, customer: bob, merchant: merchant_2)
+      invoice_3 = create(:invoice, customer: bob, merchant: merchant_2)
+      create(:transaction, invoice: invoice_2)
+      create(:transaction, invoice: invoice_3)
+
+      # merchant_3: num_transactions = 1
+      merchant_3 = create(:merchant)
+      invoice_4 = create(:invoice, customer: bob, merchant: merchant_3)
+      invoice_5 = create(:invoice, customer: bob, merchant: merchant_3)
+      invoice_6 = create(:invoice, customer: bob, merchant: merchant_3)
+      create(:transaction, invoice: invoice_4, result: 'failed')
+      create(:transaction, invoice: invoice_5, result: 'failed')
+      create(:transaction, invoice: invoice_6)
+
+      expect(bob.favorite_merchant).to eq(merchant_2)
+    end
+  end
 end
