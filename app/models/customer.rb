caf2dacb2c0ccfed9c12f5af3f3c62e2051b213f
Fenton Taylor <fentontaylor@gmail.com>
2019-10-05 16:55:12 -0600

    Refactor some queries

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,15 +9,14 @@ class Customer < ApplicationRecord
 
   def favorite_merchant
     Merchant.find_by_sql(
-      "SELECT id, name, created_at, updated_at from " +
-        "(SELECT m.*, COUNT(*) as num_transactions FROM customers c " +
+      "SELECT m.*, COUNT(*) as num_transactions FROM customers c " +
         "INNER JOIN invoices i ON c.id = i.customer_id " +
         "INNER JOIN transactions t ON t.invoice_id = i.id " +
         "INNER JOIN merchants m ON i.merchant_id = m.id " +
         "WHERE t.result = 'success' AND c.id = #{self.id} " +
         "GROUP BY m.id " +
         "ORDER BY num_transactions DESC " +
-        "LIMIT 1) temp"
+        "LIMIT 1"
     ).first
   end
 end
