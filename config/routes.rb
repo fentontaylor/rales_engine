4dc9e5fc4b5b4594dc4a6937b8d87993fe1094ae
Fenton Taylor <fentontaylor@gmail.com>
2019-10-06 09:32:37 -0600

    Add endpoint /api/v1/items/random

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
         get '/:id/best_day', to: 'best_day#show'
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/merchant', to: 'merchant#show'
+        get '/random', to: 'random#show'
       end
       resources :items, only: [:index, :show]
 
