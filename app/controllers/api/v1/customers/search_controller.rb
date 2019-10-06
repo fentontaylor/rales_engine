441b09cd53663335c3b2dfc1f798d80ad120bc25
Fenton Taylor <fentontaylor@gmail.com>
2019-10-06 10:41:10 -0600

    Add endpoints to find customers by id and first_name

---
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Customers::SearchController < ApplicationController
+  def show
+    customer = Customer.find_by( search_params )
+    render json: CustomerSerializer.new( customer )
+  end
+
+  private
+
+  def search_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
+  end
+end
--- spec/requests/api/v1/customers_search_resquest_spec.rb
+++ spec/requests/api/v1/customers_search_resquest_spec.rb
@@ -13,4 +13,17 @@ describe 'Customer search API' do
 
     expect(json['data']['attributes']['id']).to eq(c2.id)
   end
+
+  it 'can find one customer by id' do
+    c1 = create(:customer, first_name: 'Bob')
+    c2 = create(:customer, first_name: 'Sue')
+
+    get "/api/v1/customers/find?first_name=#{c2.first_name}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)
+
+    expect(json['data']['attributes']['id']).to eq(c2.id)
+  end
 end
